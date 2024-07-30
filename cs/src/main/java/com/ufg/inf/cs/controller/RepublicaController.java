ad6d1fb06fae5698245b1058c306c48a1debda3d
Lucas Nunes <lucasnunnes40@gmail.com>
2024-07-30 19:48:49 -0300

    adicionando o repository e o controller de republica

---
+++ cs/src/main/java/com/ufg/inf/cs/controller/RepublicaController.java
@@ -0,0 +1,45 @@
+package com.ufg.inf.cs.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ufg.inf.cs.models.RepublicaModel;
+import com.ufg.inf.cs.repository.RepublicaRepository;
+
+@RestController
+@RequestMapping("/republica")
+public class RepublicaController {
+    
+    @Autowired
+    private RepublicaRepository republicaRepository;
+
+    @PostMapping  
+	public ResponseEntity<RepublicaModel> salvarRepublica(@RequestBody RepublicaModel republica) {
+		RepublicaModel republicaModel = republicaRepository.save(republica);	
+	
+		return new ResponseEntity<RepublicaModel>(republicaModel, HttpStatus.OK);
+	}
+
+    @GetMapping(value = "/{id}")  
+	public ResponseEntity<RepublicaModel> buscarRepublicaPorId(@PathVariable Long id) {
+		RepublicaModel republica = republicaRepository.findById(id).get();
+	
+		return new ResponseEntity<RepublicaModel>(republica, HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<String> delete(@PathVariable Long id) {
+		republicaRepository.deleteById(id);
+		
+		return new ResponseEntity<String>("República removida com sucesso.", HttpStatus.OK);
+	}
+
+}
