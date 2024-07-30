571a4d574e4c7ef5bcba25b383910695ccf756ed
Lucas Nunes <lucasnunnes40@gmail.com>
2024-07-30 19:49:05 -0300

    ajustando repository e controller de endereco

--- cs/src/main/java/com/ufg/inf/cs/repository/EnderecoRepository.java
+++ cs/src/main/java/com/ufg/inf/cs/repository/EnderecoRepository.java
@@ -1,6 +1,10 @@
 package com.ufg.inf.cs.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ufg.inf.cs.models.EnderecoModel;
@@ -9,6 +13,9 @@ import jakarta.transaction.Transactional;
 
 @Repository
 @Transactional
-public interface EnderecoRepository extends JpaRepository<EnderecoModel, Long>{
+public interface EnderecoRepository extends JpaRepository<EnderecoModel, Long> {
+
+    @Query(value = "SELECT * FROM ENDERECOS WHERE UPPER(LOGRADOURO) LIKE UPPER(CONCAT('%', :logradouro, '%'))", nativeQuery = true)
+    ArrayList<EnderecoModel> buscarPeloLogradouro(@Param("logradouro") String logradouro);
     
 }
