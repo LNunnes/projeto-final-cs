20bf2ec515bd5628f73dbdc58f60aa9fb882d218
jntlvr <131724696+jntlvr@users.noreply.github.com>
2024-07-30 20:05:27 -0300

    Atualicação EspacoComunitario

--- cs/src/main/java/com/ufg/inf/cs/repository/EspacoComunitarioRepository.java
+++ cs/src/main/java/com/ufg/inf/cs/repository/EspacoComunitarioRepository.java
@@ -1,5 +1,13 @@
 package com.ufg.inf.cs.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface EspacoComunitarioRepository {
+import com.ufg.inf.cs.models.EspacoComunitarioModel;
 
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface EspacoComunitarioRepository extends JpaRepository<EspacoComunitarioModel, Long>{
+    
 }
--- cs/src/main/java/com/ufg/inf/cs/service/EspacoComunitarioService.java
+++ cs/src/main/java/com/ufg/inf/cs/service/EspacoComunitarioService.java
@@ -1,5 +1,33 @@
 package com.ufg.inf.cs.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ufg.inf.cs.models.EspacoComunitarioModel;
+import com.ufg.inf.cs.repository.EspacoComunitarioRepository;
+
+import java.util.List;
+
+@Service
 public class EspacoComunitarioService {
+    
+    @Autowired
+    private EspacoComunitarioRepository espacoComunitarioRepository;
+
+    public EspacoComunitarioModel salvarEspacoComunitario(EspacoComunitarioModel espacoComunitario) {
+        return espacoComunitarioRepository.save(espacoComunitario);
+    }
+
+    public EspacoComunitarioModel buscarEspacoComunitarioPorId(Long id) {
+        return espacoComunitarioRepository.findById(id).orElse(null);
+    }
+
+    public void deletarEspacoComunitarioPorId(Long id) {
+        espacoComunitarioRepository.deleteById(id);
+    }
+
+    public List<EspacoComunitarioModel> listaEspacosComunitarios() {
+        return espacoComunitarioRepository.findAll();
+    }
 
 }
